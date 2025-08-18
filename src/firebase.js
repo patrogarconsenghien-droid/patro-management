e889300d2523d12abb9bd21b99b87ff5cfb3eb48
Romain Scokart <rscokart@gmail.com>
2025-08-18 14:21:44 +0200

    Initial commit - Application Patro complète

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// REMPLACEZ cette configuration par la vôtre obtenue à l'étape 2.5
+const firebaseConfig = {
+  apiKey: "VOTRE_API_KEY",
+  authDomain: "VOTRE_PROJECT_ID.firebaseapp.com",
+  projectId: "VOTRE_PROJECT_ID",
+  storageBucket: "VOTRE_PROJECT_ID.appspot.com",
+  messagingSenderId: "VOTRE_SENDER_ID",
+  appId: "VOTRE_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
